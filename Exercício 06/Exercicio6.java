938b56b3180902cc326069699607a5b9c7ce72f4
Giovani <75649962+Giovani-Gomes@users.noreply.github.com>
2022-07-02 02:21:20 -0300

    Update Exercicio6.java

--- Exercício 06/Exercicio6.java
+++ Exercício 06/Exercicio6.java
@@ -59,7 +59,7 @@ public class Main {
                 jogada++;
             }
 
-            //imprimir tabuleiro
+            
             for (int i=0; i<jogoVelha.length; i++){
                 for (int j=0;j<jogoVelha[i].length; j++){
                     System.out.print(jogoVelha[i][j] + " | ");
